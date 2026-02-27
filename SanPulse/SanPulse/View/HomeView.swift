26ce757eee1e72920105cac42ecbaf9cfcef22bf
KEITA Modibo <Modykeita2@gmail.com>
2026-02-27 16:49:56 +0100

    nouveau text

--- SanPulse/SanPulse/View/HomeView.swift
+++ SanPulse/SanPulse/View/HomeView.swift
@@ -10,11 +10,8 @@ import SwiftUI
 struct HomeView: View {
     var body: some View {
         VStack(spacing: 24) {
-            AppointmentView()
-        
-            PatientsView()
-        
-            NotesView()
+           Text("Bonjour, Docteur 👋")
+            Spacer()
         }
         .padding()
     }
