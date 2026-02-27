6089f1c9ee9848b1c61dd1cdbe567caad02c51db
KEITA Modibo <Modykeita2@gmail.com>
2026-02-27 14:25:10 +0100

    nouveau projet

---
+++ SanPulse/SanPulse/SanPulseApp.swift
@@ -0,0 +1,17 @@
+//
+//  SanPulseApp.swift
+//  SanPulse
+//
+//  Created by Modibo on 27/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct SanPulseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
