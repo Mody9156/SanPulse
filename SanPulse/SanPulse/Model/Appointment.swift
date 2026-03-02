730ef686aee2bafe8917ebd8b978db59584737d5
KEITA Modibo <Modykeita2@gmail.com>
2026-03-02 13:10:34 +0100

    initialisation de la struct

--- SanPulse/SanPulse/Model/Appointment.swift
+++ SanPulse/SanPulse/Model/Appointment.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+struct Appointment: Identifiable {
+    let id = UUID()
+    let patientName: String
+    let type: String
+    let time: String
+    let duration: Int
+    let status: String
+}
+
+let todayAppointments: [Appointment] = [
+    Appointment(patientName: "Jean Dupont", type: "Consultation", time: "09:00", duration: 30, status: "Confirmé"),
+    Appointment(patientName: "Marie Martin", type: "Suivi", time: "11:00", duration: 45, status: "Planifié")
+]
---
+++ SanPulse/SanPulse/Model/StatItem.swift
@@ -0,0 +1,8 @@
+//
+//  StatItem.swift
+//  SanPulse
+//
+//  Created by Modibo on 02/03/2026.
+//
+
+import Foundation
