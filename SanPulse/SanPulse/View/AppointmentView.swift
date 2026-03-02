b9fb559de2b7adc86a3bc24133828eba6b585999
KEITA Modibo <Modykeita2@gmail.com>
2026-03-02 13:52:42 +0100

    ScrollView

--- SanPulse/SanPulse/View/AppointmentView.swift
+++ SanPulse/SanPulse/View/AppointmentView.swift
@@ -2,26 +2,28 @@ import SwiftUI
 
 struct AppointmentView: View {
     var body: some View {
-        VStack(alignment: .leading, spacing: 20) {
-            // HEADER
-            HStack {
-                Text("Rendez-vous")
-                    .font(.title)
-                    .bold()
-                
-                Spacer()
-                
-                Button(action: {}) {
-                    Label("Nouveau", systemImage: "plus")
-                        .padding(.horizontal, 14)
-                        .padding(.vertical, 8)
-                        .background(Color(hex: "#39855E"))
-                        .foregroundColor(.white)
-                        .clipShape(Capsule())
+        ScrollView {
+            VStack(alignment: .leading, spacing: 20) {
+                // HEADER
+                HStack {
+                    Text("Rendez-vous")
+                        .font(.title)
+                        .bold()
+                    
+                    Spacer()
+                    
+                    Button(action: {}) {
+                        Label("Nouveau", systemImage: "plus")
+                            .padding(.horizontal, 14)
+                            .padding(.vertical, 8)
+                            .background(Color(hex: "#39855E"))
+                            .foregroundColor(.white)
+                            .clipShape(Capsule())
+                    }
                 }
             }
+            .padding()
         }
-        .padding()
     }
 }
 
