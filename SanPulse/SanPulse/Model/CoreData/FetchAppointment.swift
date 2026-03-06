df11fbb4791f7147df9c396fe5f8f8fb3fe0644b
KEITA Modibo <Modykeita2@gmail.com>
2026-03-06 18:44:15 +0100

    nouvelle variable

--- SanPulse/SanPulse/Model/CoreData/FetchAppointment.swift
+++ SanPulse/SanPulse/Model/CoreData/FetchAppointment.swift
@@ -6,7 +6,29 @@
 //
 
 import Foundation
+import CoreData
+import Observation
+import SwiftUI
 
+@Observable
 class FetchAppointment {
+    var context : NSManagedObjectContext
+    
+    init(
+        context: NSManagedObjectContext = CoreDataStack.share.PersistentContainer.viewContext
+    ) {
+        self.context = context
+    }
+    
+    func addAppointement(duration:Int,
+                         PatientName:String,
+                         time:Date,
+                         type:String) throws {
+        
+         context.performAndWait {
+            let appointment = Appointment(context: context)
+             
+        }
+    }
     
 }
